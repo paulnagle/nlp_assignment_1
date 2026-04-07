e5a7c74105254bdbbdec51f18cc88490d58f2dc6
Paul Nagle <paulnagle@gmail.com>
2026-04-07 18:22:08 +0100

    formatting

--- data/mcq_questions/count.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/mcq_questions/mc_questions_file-1.csv')
-print(f"Total questions: {len(df)}")
-print(f"Questions by country:\n{df['country'].value_counts()}")
